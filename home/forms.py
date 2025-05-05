7c5f011d2b3ec98971a61b4356e05d96f196bf52
Ramiro Cordoba <ramirocr27@gmail.com>
2025-05-05 12:58:59 -0300

    Primer commit: Base del proyecto

---
+++ home/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+class RegistroForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    password1 = forms.CharField(label="Contraseña", widget=forms.PasswordInput())
+    password2 = forms.CharField(
+        label="Repetir Contraseña", widget=forms.PasswordInput()
+    )
+
+    class Meta:
+        model = User
+        fields = [
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+            "password1",
+            "password2",
+        ]
---
+++ home/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from .models import *
+from .forms import *
+
+
+#--- Pagina principal
+def home_view(request):
+  return render(request,"home/index.html")
+
+#--- Pagina para el login
+def login_view(request):
+  if request.method == "POST":
+    usuario = request.POST['username']
+    clave = request.POST['password']
+    user=authenticate(request, username=usuario,password=clave)
+    if user is not None:
+      login(request, user)
+      return redirect('dashboard') # Si esta todo bien, lleva al dashboard
+    else:
+      return redirect(reverse_lazy('home'))
+  else:
+    #___ Si ingresa por aca es porque es la primera vez.
+    miForm= AuthenticationForm()
+  return render(request, "home/login.html",{"form":miForm})
+
+#--- Registro de usuario
+def register(request):
+    if request.method == "POST":
+        miForm = RegistroForm(request.POST)
+
+        if miForm.is_valid():
+            usuario = miForm.cleaned_data.get("username")
+            miForm.save()
+            return redirect(reverse_lazy("home"))
+    else:
+        # __ Si ingresa en el else es la primera vez
+        miForm = RegistroForm()
+
+    return render(request, "home/registro.html", {"form": miForm})
