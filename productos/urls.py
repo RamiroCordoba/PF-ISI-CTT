eaefa6ba565a93ce6b1416227bdf84fe9781396f
Ramiro Cordoba <ramirocr27@gmail.com>
2025-05-25 20:04:17 -0300

    CRUD de productos y mejoras esteticas en general

--- productos/urls.py
+++ productos/urls.py
@@ -3,8 +3,14 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    #__________ Categorias
     path('categorias/', CategoriaList.as_view(), name='categorias'),
     path('categorias/nueva_categoria/', CategoriaCreate.as_view(), name='nueva_categoria'),
     path('categorias/editar_categoria/<int:pk>/', CategoriaUpdate.as_view(), name='editar_categoria'),
     path('categorias/eliminar_categoria/<int:pk>/', CategoriaDelete.as_view(), name='eliminar_categoria'),
+    #__________ Productos
+    path('productos/misproductos', ProductoList.as_view(), name='misproductos'),
+    path('productos/nuevo_producto/', ProductoCreate.as_view(), name='nuevo_producto'),
+    path('productos/editar_producto/<int:pk>/', ProductoUpdate.as_view(), name='editar_producto'),
+    path('productos/eliminar_producto/<int:pk>/', ProductoDelete.as_view(), name='eliminar_producto'),
 ]
--- productos/views.py
+++ productos/views.py
@@ -35,4 +35,47 @@ class CategoriaDelete(DeleteView):
      model=Categoria
      template_name="categorias/categoria_confirm_delete.html"
      success_url = reverse_lazy("categorias")
-#______ Productos
+
+#______ Productos CRUD
+
+class ProductoList(ListView):
+    model = Producto
+    template_name = "productos/producto_list.html"
+    context_object_name = "productos"
+
+    def get_queryset(self):
+        queryset = super().get_queryset().order_by("id")
+        buscar = self.request.GET.get("buscar")
+        categoria_id = self.request.GET.get("categoria")
+
+        if buscar:
+            queryset = queryset.filter(nombre__icontains=buscar)
+
+        if categoria_id:
+            queryset = queryset.filter(categoria_id=categoria_id)
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        from .models import Categoria
+        context["categorias"] = Categoria.objects.all().order_by("nombre")
+        return context
+
+  
+class ProductoCreate(CreateView):
+     model=Producto
+     fields=["nombre","descripcion","precio","stock","stock_maximo","stock_minimo","categoria","marca","fecha_ultimo_ingreso","activo"]
+     template_name="productos/producto_form.html"
+     success_url = reverse_lazy("misproductos")
+     
+class ProductoUpdate(UpdateView):
+     model=Producto
+     fields=["nombre","descripcion","precio","stock","stock_maximo","stock_minimo","categoria","marca","fecha_ultimo_ingreso","activo"]
+     template_name="productos/producto_form.html"
+     success_url = reverse_lazy("misproductos")
+
+class ProductoDelete(DeleteView):
+     model=Producto
+     template_name="productos/producto_confirm_delete.html"
+     success_url = reverse_lazy("misproductos")
