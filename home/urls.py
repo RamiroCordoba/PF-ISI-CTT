ec2342582b59792035d70fdfd092f21d858df0c1
Ramiro Cordoba <ramirocr27@gmail.com>
2025-06-29 17:10:40 -0300

    Personalizacion mail recuperar contraseña.

--- home/urls.py
+++ home/urls.py
@@ -7,10 +7,15 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     # Recuperación de contraseña
     path(
-        'password_reset/',
-        auth_views.PasswordResetView.as_view(template_name='restablecedor/password_reset_form.html'),
-        name='password_reset'
+    'password_reset/',
+    auth_views.PasswordResetView.as_view(
+        template_name='restablecedor/password_reset_form.html',
+        email_template_name='restablecedor/password_reset_email.txt',         # versión texto plano por si tiene bloqueado HTML el usuario
+        html_email_template_name='restablecedor/password_reset_email.html',   # <- versión moderna con HTML
+        subject_template_name='restablecedor/password_reset_subject.txt'
     ),
+    name='password_reset'
+),
     path(
         'password_reset/done/',
         auth_views.PasswordResetDoneView.as_view(template_name='restablecedor/password_reset_done.html'),
