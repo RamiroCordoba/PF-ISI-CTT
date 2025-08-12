2582636c5e2701ffc637ef1677623163f8c75bc8
TomboliniSantiago <santitombo@gmail.com>
2025-08-12 20:21:04 -0300

    adding Venta Class. Edited Linea_Pedido to LineaPedido (to maintain consistency between other classnames in other modules)

--- ventas/models.py
+++ ventas/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from productos.models import Producto
 
-class Linea_Pedido(models.Model):
+class LineaPedido(models.Model):
     producto = models.ForeignKey(Producto, on_delete=models.PROTECT, related_name='lineas_pedido')
     cantidad = models.PositiveIntegerField()
     observacion = models.CharField(max_length=250, blank=True, null=True)
@@ -13,4 +13,26 @@ class Linea_Pedido(models.Model):
 
     def __str__(self):
         return f'Producto: {self.producto.nombre} - Cantidad: {self.cantidad} - Precio Unitario: {self.producto.precio}'
+    
+
+    
+class Venta(models.Model):
+    observaciones = models.CharField(max_length=50, blank=True, null=True)
+    completa = models.BooleanField(default=False)
+    fecha = models.DateTimeField(auto_now_add=True)
+    formaPago = models.CharField(max_length=50, choices=[
+        ('EFECTIVO', 'Efectivo'),
+        ('CREDITO', 'Crédito'),
+        ('DEBITO', 'Débito'),
+        ('TRANSFERENCIA', 'Transferencia'),
+    ])
+    nombreCliente = models.CharField(max_length=60, blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'venta'
+        verbose_name_plural = 'ventas'
+        ordering = ['-fecha']  # Ordeno por fecha descendente
+
+    def __str__(self):
+        return f'Venta ID: {self.id} - Cliente: {self.nombreCliente or "Consumidor Final"} - Fecha: {self.fecha.strftime("%d/%m/%Y")}'
     
