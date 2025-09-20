9d503d05946a5fc1f59bcbf2cf15cafee4430c2e
Ramiro Cordoba <ramirocr27@gmail.com>
2025-09-20 16:36:02 -0300

    Primeros pasos del reporte de ventas generales.

--- reporte/urls.py
+++ reporte/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard_view, name='base_para_informes'),
+    path('reportes/rep_ventas_generales', views.rep_ventas_generales_view, name='rep_ventas_generales'),
 ]
--- reporte/views.py
+++ reporte/views.py
@@ -2,3 +2,17 @@ from django.shortcuts import render
 
 def dashboard_view(request):
     return render(request, 'reportes/index_report.html')
+
+def rep_ventas_generales_view(request):
+    # Obtener parámetros GET
+    fecha_desde = request.GET.get('fecha_desde', '')
+    fecha_hasta = request.GET.get('fecha_hasta', '')
+    vendedores = request.GET.getlist('vendedores')
+    categorias = request.GET.getlist('categorias')
+    context = {
+        'fecha_desde': fecha_desde,
+        'fecha_hasta': fecha_hasta,
+        'vendedores': vendedores,
+        'categorias': categorias,
+    }
+    return render(request, 'reportes/rep_ventas_generales.html', context)
