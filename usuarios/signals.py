ec568ff743b14b6a0ad09390b67d4a27f2ecf3f6
Ramiro Cordoba <ramirocr27@gmail.com>
2025-10-11 17:35:04 -0300

    Revert "Ramiro (#54)"
    This reverts commit c90d6dd761ff083446d4dedcae66568f9a2d2da0.

--- usuarios/signals.py
+++ usuarios/signals.py
@@ -3,7 +3,6 @@ from django.db.models.signals import post_migrate
 from django.dispatch import receiver
 from django.contrib.contenttypes.models import ContentType
 from productos.models import Producto, Categoria
-from django.contrib.auth import get_user_model
 
 @receiver(post_migrate)
 def crear_grupos(sender, **kwargs):
@@ -18,16 +17,3 @@ def crear_grupos(sender, **kwargs):
         codename__startswith='view_'
     )
     vendedor.permissions.set(permisos_vendedor)
-
-    # Ajustar is_staff según grupo asignado
-    User = get_user_model()
-    # Vendedores: is_staff = False
-    for user in User.objects.filter(groups__name='vendedor'):
-        if user.is_staff:
-            user.is_staff = False
-            user.save(update_fields=['is_staff'])
-    # Administradores: is_staff = True
-    for user in User.objects.filter(groups__name='administrador'):
-        if not user.is_staff:
-            user.is_staff = True
-            user.save(update_fields=['is_staff'])
